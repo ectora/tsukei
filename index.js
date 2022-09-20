d3c0e514f08a8b7dcf4022657bad8fdcc55d193e
Euris <teamtdm567@gmail.com>
2022-09-20 09:31:07 +0800

    edit feature

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-const { Client, WebhookClient } = require('discord.js-selfbot-v13');
+require('dotenv').config();
+const { Client, WebhookClient, MessageEmbed } = require('discord.js-selfbot-v13');
 const client = new Client({ checkUpdate: false });
 const hook = new WebhookClient({
       url: process.env.WEBHOOK
 });
 
 client.on('ready', () => client.user.setStatus('invisible'));
+
+client.on('messageUpdate', async (oldMsg, newMsg) => {
+      hook.send({
+            embeds: [
+                  new MessageEmbed()
+                        .setColor('BLURPLE')
+                        .setAuthor({ name: `${message.author.tag} (${message.author.id})`, iconURL: newMsg.author.displayAvatarURL() })
+                        .addFields([
+                              { name: 'Before', value: oldMsg.content },
+                              { name: 'After', value: newMsg.content }
+                        ])
+            ]
+      });
+});
+
 client.on('messageCreate', async (message) => {
       if (message.author.bot || message.channel.type !== 'GUILD_TEXT') return;
       if (message.guild.id !== process.env.TARGET_GUILD) return;
